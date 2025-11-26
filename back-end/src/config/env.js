36410a768d57e99948b6168170c02da3a58b0fe1
unknown <nurdiansyahagung277@gmail.com>
2025-11-26 10:26:16 +0700

    Update: postman testing and collection

--- back-end/src/config/env.js
+++ back-end/src/config/env.js
@@ -6,7 +6,7 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config({ path: join(__dirname, '..', '.env') });
+dotenv.config({ path: join(__dirname, '..', '..', '.env') });
 
 const config = {
   env: process.env.NODE_ENV || 'development',
